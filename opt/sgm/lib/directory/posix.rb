c4d09ee60abfa425448a05c52b9bd143279e60fa
Tareq <saif.tareq@gmail.com>
2025-10-06 07:13:08 +0000

    Add POSIX additive and sync output mappings

--- opt/sgm/lib/directory/posix.rb
+++ opt/sgm/lib/directory/posix.rb
@@ -30,9 +30,52 @@ module Sgm::Directory::POSIX
       attribute = @options.css('user-unique-attribute').text
       members.each do |member|
         record = passwd.select {|record| record[0] == member}.first
+        next if record.nil?
         retval.push record[attribute.to_i]
       end
       return retval
+    end
+
+    def group_exists?(group_directory_id)
+      @connection.exec!('cat /etc/group').split.map {|line| line.split(":")}.select {|record| record[0] == group_directory_id}.count > 0
+    end
+
+    def select_valid_members(members)
+      users = @connection.exec!('cat /etc/passwd').split.map {|line| line.split(":").first}
+      members
+        .map {|el| el.member_id}
+        .select {|member| users.include?(member)}
+    end
+
+    def ensure_group(group_directory_id)
+      unless group_exists? group_directory_id
+        @connection.exec!("sudo groupadd '#{group_directory_id}'")
+      end
+    end
+
+    def add_members(group_directory_id, members)
+      ensure_group group_directory_id
+      _members = members - get_members(group_directory_id)
+      if _members.count == 0
+        puts "Additive: Noop adding members to '#{group_directory_id}'"
+        return
+      end
+      _members.each do |_member|
+        @connection.exec!("sudo usermod -aG '#{group_directory_id}' '#{_member}'")
+      end
+    end
+
+    def sync_members(group_directory_id, members)
+      ensure_group group_directory_id
+      existing_members = get_members(group_directory_id)
+      members_to_add = members - existing_members
+      members_to_remove = existing_members - members
+      members_to_add.each do |member|
+        @connection.exec!("sudo usermod -aG '#{group_directory_id}' '#{member}'")
+      end
+      members_to_remove.each do |member|
+        @connection.exec!("sudo usermod -rG '#{group_directory_id}' '#{member}'")
+      end
     end
 
   end
